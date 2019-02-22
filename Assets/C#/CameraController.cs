a7d1482d666ba10c7b559392338d656801bb085a
kensei74470 <acousticversion@docomo.ne.jp>
2019-02-22 15:59:46 +0900

    add tab

--- Assets/C#/CameraController.cs
+++ Assets/C#/CameraController.cs
@@ -11,8 +11,8 @@ public class CameraController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 playerPos = player.transform.position;
-        transform.position = new Vector3(playerPos.x,transform.position.y, transform.position.z);
+	        Vector3 playerPos = player.transform.position;
+        	transform.position = new Vector3(playerPos.x,transform.position.y, transform.position.z);
 
 	}
 }
